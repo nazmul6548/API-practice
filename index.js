d34557ade261c7f4df13596361c25e6203a617ab
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-26 22:32:47 +0600

    task

--- index.js
+++ index.js
@@ -5,7 +5,29 @@ function amarApi() {
 };
 
 function display(data) {
+    const ids = document.getElementById('ulClass')
     for (const i of data) {
-        console.log(i.username);
+        // console.log(i.username);
+        const li =document.createElement('li');
+        li.innerText = i.username;
+        ids.appendChild(li);
     }
 }
+
+function genjam(data) {
+    const ids = document.getElementById('ulClass')
+    for (const i of data) {
+        // console.log(i.username);
+        const li =document.createElement('li');
+        li.innerText = i.name;
+        ids.appendChild(li);
+    }
+}
+// function beyadob(data) {
+//     const data = document.getElementById('ulClass');
+//     for (const iterator of data) {
+//         const li = document.createElement("li");
+//         li.innerText=iterator.street;
+//         data.appendChild(li);
+//     }
+// }
